42faf149f76629be046cdc345ad49cf5d011ae63
Aleksey Videman <alekseyvideman@gmail.com>
2026-05-24 12:21:33 +0700

    Сверстал сайт для пк и мобилы

---
+++ src/routes/Footer.svelte
@@ -0,0 +1,24 @@
+<footer>
+    <p>
+        ИП Видеман Алексей Валентинович | ИНН 244501489528 | ОГРНИП
+        326246800086773
+    </p>
+</footer>
+
+<style>
+    footer {
+        height: 150px;
+        margin-top: auto;
+        padding: 0 0 0 var(--global-left-right-padding-dekstop);
+        border-top: solid 1px #939393;
+    }
+    p {
+        width: 50vw;
+    }
+
+    @media (max-width: 391px) {
+        footer {
+            padding: 0 0 0 var(--global-left-right-padding-mobile);
+        }
+    }
+</style>
