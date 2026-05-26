7d33758a9fbb4dc9058d9ce50c1b201a92a2e5a5
Aleksey Videman <alekseyvideman@gmail.com>
2026-05-26 10:35:52 +0700

    add 404 page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,13 @@
+<svelte:head>
+    <title>Неизвестная</title>
+</svelte:head>
+
+<center><h2>Страница не найдена</h2></center>
+<center><a href="/">Вернуться на главную</a></center>
+
+<style>
+    a {
+        color: black;
+        background-color: white;
+    }
+</style>
